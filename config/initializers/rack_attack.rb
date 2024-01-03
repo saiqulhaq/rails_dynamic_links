027b89ec74d2b4dc83f4851a8cff3f4bbf84cfab
Saiqul Haq <saiqulhaq@gmail.com>
2024-01-03 23:58:06 +0700

    feat: throttle by IP for DynamicLinks#create API

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,4 +1,6 @@
 # Throttle requests to 5 requests per 2 seconds by IP
 Rack::Attack.throttle("requests by ip", limit: 5, period: 2) do |request|
-  request.ip
+  if request.path == "/v1/shortLinks" && request.post?
+    request.ip
+  end
 end
